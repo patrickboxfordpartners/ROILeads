9e422c059ef76cdb85b94c809e8223938e1fb745
patrickboxfordpartners <patrick@boxfordpartners.com>
2025-11-25 20:13:07 -0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,8 @@ import os
 import sys
 
 # Add the 'backend' folder to the Python path
+# This allows 'from app.libs...' to work
 sys.path.append(os.path.join(os.path.dirname(__file__), '../backend'))
 
-# Now we can import your existing app instance
+# Now import the app
 from app.main_prod import app
